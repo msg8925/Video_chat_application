c547cf8b37a3ccb627b493487537b8845e0de456
msg8925 <msg.8925@gmail.com>
2022-12-01 12:51:21 +0700

    JS file connected

---
+++ mychat/static/js/streams.js
@@ -0,0 +1,5 @@
+const APP_ID = '673115882376428ba37ef97e93fa71f2'
+const CHANNEL = 'main'
+const TOKEN = '007eJxTYND87uQqa3t7JWu/fwa7xLVfjzcbdP1fy7PoobGhwQFtaRkFBjNzY0NDUwsLI2NzMxMji6REY/PUNEvzVEvjtERzwzSjj3YdyQ2BjAx7LpYwMTJAIIjPwpCbmJnHwAAAsw8dxg=='
+
+console.log('Stream.js connected')
