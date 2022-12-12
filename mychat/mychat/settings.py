c6cafb422046f50f186801392da68845daddce69
msg8925 <msg.8925@gmail.com>
2022-12-12 14:10:32 +0700

    Update settings.py

--- mychat/mychat/settings.py
+++ mychat/mychat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pjao=q!b0&z@5*oaex21is(npz85tcx+c+t5lxgy@%7qx&@0j!'
+SECRET_KEY = os.getenv("DJANGO-SECRET-KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
